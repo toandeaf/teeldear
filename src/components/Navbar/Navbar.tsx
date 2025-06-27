ead2f305241fbfe465337559d465781cb790be85
jaketoan <jaketoan@hotmail.co.uk>
2025-06-27 13:19:21 +0100

    Updating look

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,13 @@
 import './Navbar.css'
+import {
+  Bell,
+  Settings,
+  Calendar,
+  Users,
+  Archive,
+  FolderOpen,
+  CheckSquare,
+} from 'lucide-react'
 
 const Navbar = () => {
   return (
@@ -7,15 +16,36 @@ const Navbar = () => {
         <div className="logo">TL;DR</div>
         <div className="neu-divider"></div>
 
-        <a href="#">Tasks</a>
-        <a href="#">Team</a>
-        <a href="#">Calendar</a>
-        <a href="#">Notifications</a>
+        <a href="#">
+          <CheckSquare size={20} />
+          Tasks
+        </a>
+        <a href="#">
+          <Users size={20} />
+          Team
+        </a>
+        <a href="#">
+          <Calendar size={20} />
+          Calendar
+        </a>
+        <a href="#">
+          <Bell size={20} />
+          Notifications
+        </a>
         <div className="neu-divider"></div>
 
-        <a href="#">Projects</a>
-        <a href="#">Archives</a>
-        <a href="#">Settings</a>
+        <a href="#">
+          <FolderOpen size={20} />
+          Projects
+        </a>
+        <a href="#">
+          <Archive size={20} />
+          Archives
+        </a>
+        <a href="#">
+          <Settings size={20} />
+          Settings
+        </a>
       </div>
       <div className="footer"></div>
     </div>
